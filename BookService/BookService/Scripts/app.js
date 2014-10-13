00ca8ab5f101244952a2ac2502d77e6c5002dfa0
ilyas-incubasys <ilyas.khan@incubasys.com>
2014-10-13 11:45:20 +0500

    Alert commit

--- BookService/BookService/Scripts/app.js
+++ BookService/BookService/Scripts/app.js
@@ -15,7 +15,7 @@
 
     var booksUri = '/api/books/';
     var authorsUri = '/api/authors/';
-    
+    alert('');
     function ajaxHelper(uri, method, data) {
         self.error(''); // Clear error message
         return $.ajax({
